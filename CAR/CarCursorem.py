ff9ae37da2f31aebf724d6c5cda32cf67e1f91c7
unknown <alexapbabiera@iskolarngbayan.pup.edu.ph>
2023-07-05 03:07:16 +0800

    Pseudocode for Car Cursorem

--- CAR/CarCursorem.py
+++ CAR/CarCursorem.py
@@ -1,3 +1,7 @@
 '''Pseudocode'''
+#import car class
+#create object for car
+# call the accelerate method five times and display current speed (using FOR-LOOP)
+# call brake method five times and display current speed (USING FOR-LOOP)
 
-# Create Car class
+
