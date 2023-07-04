67d42378436c65fcb86b5eef2c8b887837ff8be6
unknown <alexapbabiera@iskolarngbayan.pup.edu.ph>
2023-07-05 02:12:42 +0800

    Defining Object 1(FAN 1) Attributes and Displaying Function

--- FanCursorem.py
+++ FanCursorem.py
@@ -6,4 +6,10 @@ def FAN1_RUN ():
 # create object
   FAN1 = FAN()
   
-  
+# display object's attributes
+#Object 1 Attributes
+  FAN1.set_speed(3)
+  FAN1.set_radius(10)
+  FAN1.set_color('Yellow')
+  FAN1.set_status(True)
+  FAN1.fan1_attributes()
