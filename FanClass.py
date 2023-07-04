34ffa7a22c01ecf9193a4f603d4b10ba762e0160
unknown <alexapbabiera@iskolarngbayan.pup.edu.ph>
2023-07-04 22:25:30 +0800

    Setting FAN attributes and making it private

--- FanClass.py
+++ FanClass.py
@@ -6,9 +6,22 @@ class FAN:
         self.__radius = radius
         self.__color = color
         self.__on = on
+        
 # define methods
 # create private int data for speed
+    def set_speed(self, speed):
+        self.__speed = speed
+        
 # create private bool data for power(ON/OFF)
-# create private float data for radius
+    def set_status(self, power):
+        self.__power = bool(power)
+        
+# create private float data for radius        
+    def set_radius(self, radius):
+        self.__radius = float(radius)
+        
 # create private string data for color
+    def set_color(self, color):
+        self.__color = str(color) 
+        
 # create getters
