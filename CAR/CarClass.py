deac3cd9ffa19c869e52e5db07f35b823008e2ce
unknown <alexapbabiera@iskolarngbayan.pup.edu.ph>
2023-07-05 03:14:21 +0800

    Created a Car Class and its constructor

--- CAR/CarClass.py
+++ CAR/CarClass.py
@@ -1,6 +1,11 @@
 '''PSEUDOCODE'''
 # Create Car class
+class Car:
 # init/create constructor
+    def __init__ (self, year_model = 2020, make = "Chevrolet Corvette", speed=0):
+        self.__year_model = year_model 
+        self.__make = make
+        self.__speed = speed
 #Create method for car's acceleration (+5 speed) and brake (-5) whenever called
 #get car's speed
 #display the car's speed
