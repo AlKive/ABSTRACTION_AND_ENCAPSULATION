134fc23c35f800aa1d91de65053c929d7be86249
unknown <alexapbabiera@iskolarngbayan.pup.edu.ph>
2023-07-05 03:38:46 +0800

    Called the accelerate method five times and displaying the resulting speed

--- CAR/CarCursorem.py
+++ CAR/CarCursorem.py
@@ -4,6 +4,9 @@ from CarClass import CAR
 #create object for car
 Chevrolette = CAR()
 # call the accelerate method five times and display current speed (using FOR-LOOP)
+for i in range (5):
+    Chevrolette.accelerate()
+    Chevrolette.display_current_speed()
 # call brake method five times and display current speed (USING FOR-LOOP)
 
 
