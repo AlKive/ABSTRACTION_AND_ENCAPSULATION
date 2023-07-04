598c800b3cedf84023196c80e556a963fcbb5f49
unknown <alexapbabiera@iskolarngbayan.pup.edu.ph>
2023-07-05 07:09:22 +0800

    Created setter methods

--- PET/PetClass.py
+++ PET/PetClass.py
@@ -8,6 +8,15 @@ class PET:
         self.__age = age
 #create methods
    #setters
+def set_name(self, name):
+        self.__name = name
+
+def set_type(self, animal_type):
+        self.__animal_type = animal_type
+
+def set_age(self, age):
+        self.__age = age
+        
    #getters
    #display
 
