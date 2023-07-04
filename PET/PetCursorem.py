3ff728b00961d6fbb307880fe96a1b2267f83f0c
unknown <alexapbabiera@iskolarngbayan.pup.edu.ph>
2023-07-05 07:18:54 +0800

    Input function

--- PET/PetCursorem.py
+++ PET/PetCursorem.py
@@ -1,5 +1,9 @@
 #import PET class
 from PetClass import PET
 #user input prompts
+pet_name = input("Enter your pet's name : ")
+pet_type = input("Enter your pet's animal type: ") 
+pet_age = input("Enter your pet's age : ")
+
 #create PET object
 #display output
