1d8a297eac16620021d9c77b1b65a5301674f7e5
unknown <alexapbabiera@iskolarngbayan.pup.edu.ph>
2023-07-05 07:31:47 +0800

    Created PET object and displayed the output: the code is working!

--- PET/PetClass.py
+++ PET/PetClass.py
@@ -1,37 +1,37 @@
 
 #create class
 class PET:
- def __init__(self, name, animal_type, age):
+    def __init__(self, name, animal_type, age):
         # instance variables
         self.__name = name
         self.__animal_type = animal_type
         self.__age = age
 #create methods
    #setters
-def set_name(self, name):
-        self.__name = name
+    def set_name(self, name):
+            self.__name = name
 
-def set_type(self, animal_type):
-        self.__animal_type = animal_type
+    def set_type(self, animal_type):
+            self.__animal_type = animal_type
 
-def set_age(self, age):
-        self.__age = age
+    def set_age(self, age):
+            self.__age = age
+            
+    #getters
+    def get_name(self):
+            return self.__name
         
-   #getters
-def get_name(self):
-        return self.__name
+        # get animal type
+    def get_type(self):
+            return self.__animal_type
+        
+        # get age
+    def get_age(self):
+            return self.__age
+    #display
     
-    # get animal type
-def get_type(self):
-        return self.__animal_type
-    
-    # get age
-def get_age(self):
-        return self.__age
-   #display
-   
-def display(self):
-        print("  Pet's Name  :", self.__name)
-        print("  Animal Type :" , self.__animal_type)
-        print("  Pet's Age   :" , self.__age)
+    def display(self):
+            print("  Pet's Name  :", self.__name)
+            print("  Animal Type :" , self.__animal_type)
+            print("  Pet's Age   :" , self.__age)
 
--- PET/PetCursorem.py
+++ PET/PetCursorem.py
@@ -6,4 +6,6 @@ pet_type = input("Enter your pet's animal type: ")
 pet_age = input("Enter your pet's age : ")
 
 #create PET object
+PET_INFO = PET(pet_name, pet_type, pet_age)
 #display output
+PET_INFO.display()
