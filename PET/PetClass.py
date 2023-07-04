7b30779725fac145e91cfdd327343c80e93f8528
unknown <alexapbabiera@iskolarngbayan.pup.edu.ph>
2023-07-05 07:07:34 +0800

    Created a PET class and its constructor

--- PET/PetClass.py
+++ PET/PetClass.py
@@ -1,6 +1,11 @@
-'''Pseudocode'''
+
 #create class
-#init/constructor
+class PET:
+ def __init__(self, name, animal_type, age):
+        # instance variables
+        self.__name = name
+        self.__animal_type = animal_type
+        self.__age = age
 #create methods
    #setters
    #getters
